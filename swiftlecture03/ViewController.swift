0af618df0e77c9144c8860709e4d55916af30a3a
zjl <644134599@qq.com>
2018-07-23 16:02:38 +0800

    23-2

--- swiftlecture03/ViewController.swift
+++ swiftlecture03/ViewController.swift
@@ -10,21 +10,61 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet var lbName: UIView!
+    
+    @IBOutlet weak var lbName: UILabel!
+    
+    
+    @IBOutlet weak var switchStatus: UISwitch!
+    
+    
+    @IBOutlet weak var scoreSlider: UISlider!
+    
+    
+    @IBOutlet weak var addScoreStepper: UIStepper!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-    @IBOutlet var btName: [UIButton]!
+
     
     @IBAction func btName2(_ sender: UIButton) {
-        lbName.text="anne"
+      lbName.text = "anne"
+        sender.setTitle("ok", for: UIControl.State.normal)
+      
     }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        print("touchesBegan")
+//        if let touch=touches.first{
+//            let loc=touch.location(in: self.touchView)
+//            print("\(loc)")
+//            if(self.touchView.frame.contains(loc)){
+//                self.touchView.backgroundColor=UIColor.red
+//            }else{
+//                self.touchView.backgroundColor=UIColor.blue
+//
+//            }
+//       }
+//    }
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print("touchesEnded")
+        
+    }
+    
+    @IBAction func stepperChanged(_ sender: UIStepper) {
+        print("\(addScoreStepper.value)")
+        scoreSlider.setValue(Float(addScoreStepper.value), animated: false)
+    }
+    
+    
+    
 }
-
